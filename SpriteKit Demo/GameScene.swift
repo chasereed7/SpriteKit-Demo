379f69c8a88387c53a3cb877ae3b4cd099b58fee
Carter <carter.reed7@iCloud.com>
2019-10-16 20:18:10 -0500

    made a updating program that can tell you where things are messed up in the console

--- SpriteKit Demo/GameScene.swift
+++ SpriteKit Demo/GameScene.swift
@@ -5,40 +5,34 @@
 //  Created by Carter Reed on 10/5/19.
 //  Copyright © 2019 Carter Reed. All rights reserved.
 //
- 
+
 import SpriteKit
 
 class GameScene: SKScene {
     
-    let dogSpriteNode = SKSpriteNode(imageNamed: "Run0")
-    var dogFrames = [SKTexture]()
-    
-    
     override func didMove(to view: SKView) {
-        dogSpriteNode.position = CGPoint(x: frame.midX, y: frame.midY)
-        addChild(dogSpriteNode)
         
-        let textureAtlas = SKTextureAtlas(named: "Dog Frames")
-   
-        for index in 0..<textureAtlas.textureNames.count {
-            let textureName = "Run" + String(index)
-            dogFrames.append(textureAtlas.textureNamed(textureName))
-        }
     }
     
-    
-
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if !dogSpriteNode.hasActions() {
-        dogSpriteNode.run(SKAction.repeatForever(SKAction.animate(with: dogFrames, timePerFrame: 0.1)))
-        } else {
-            dogSpriteNode.removeAllActions()
-        }
-    
+    override func update(_ currentTime: TimeInterval) {
+        print("1")
     }
     
+    override func didEvaluateActions() {
+        print("2")
+    }
     
+    override func didSimulatePhysics() {
+        print("3")
+    }
     
-}
-
-
+    override func didApplyConstraints() {
+        print("4")
+    }
+    
+    override func didFinishUpdate() {
+        print("5")
+        isPaused = true
+    }
+    
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                }
